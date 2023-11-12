526548bfde22dba2c20b9499a6616fc0b2b79c9d
naftalimurgor <murgornaftali@gmail.com>
2023-11-12 17:21:38 +0300

    add start script and preinstall scripts

--- bitgesell-node-js-example/src/server.js
+++ bitgesell-node-js-example/src/server.js
@@ -1,16 +1,44 @@
 const express = require('express')
+const jsbgl = require('./jsbgl/src/jsbgl')
+async function initJsbglModule(scope) {
+    try {
+        await jsbgl.asyncInit(scope)
+    } catch (error) {
+        console.log(error)
+    }
+}
+async function createWallet() {
+    try {
+        const wallet = new globalThis.Wallet({
+            path_type: 'BIP84',
+            testnet: false,
+        })
+        return JSON.stringify(wallet)
+    } catch (error) {
+        return null
+    }
+}
 
 const main = async () => {
+    try {
+        await initJsbglModule(globalThis)
+    } catch (error) {
+        console.log(error);
+    }
     const app = express()
-    const port = 3000
+    const port = process.env.PORT || 3000
 
     app.use(express.json())
 
     app.get('/', (req, res) => res.send('Hello World!'))
 
-    app.get('/create-wallet', (req, res) => {
-        console.log(req.body)
-        res.json({ success: true })
+    app.get('/create-wallet', async (req, res) => {
+        try {
+            const wallet = createWallet()
+            res.json({ sucess: true, wallet: wallet })
+        } catch (error) {
+            res.json({ msg: "failed" }).status(500)
+        }
     })
 
     app.listen(port, () => console.log(`Example app listening at http://localhost:3000`))
