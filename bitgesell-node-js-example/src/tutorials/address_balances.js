d9ea1a8f78205963cc58d9cac6e25a05cb948a2c
naftalimurgor <murgornaftali@gmail.com>
2023-11-15 16:45:28 +0300

    [wip]: address balance tutorial

---
+++ bitgesell-node-js-example/src/tutorials/address_balances.js
@@ -0,0 +1,7 @@
+// talk to api
+// get balance from endpoint for an address
+const BITGESELL_NODE_RPC = '';
+const BITGESELL_EXPLOER_API = 'https://api.bitaps.com/bgl/v1/blockchain'
+
+
+// hit api endpoints
