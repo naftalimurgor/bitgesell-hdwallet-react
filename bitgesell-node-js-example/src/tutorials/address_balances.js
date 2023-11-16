2bbd5d5de4e81df0861b62b8e3f84a22b27c1cc0
naftalimurgor <murgornaftali@gmail.com>
2023-11-16 16:59:43 +0300

    add working example for address_balance tutorial

--- bitgesell-node-js-example/src/tutorials/address_balances.js
+++ bitgesell-node-js-example/src/tutorials/address_balances.js
@@ -1,8 +1,10 @@
-// talk to api
-// get balance from endpoint for an address
-const axios = require('axios')
-const BITGESELL_NODE_RPC = '';
-const BITGESELL_EXPLOER_API = 'https://api.bitaps.com/bgl/v1/blockchain'
+/**
+ * In this example we talk to an api to get balance of an address
+ */
+
+const axios = require('axios');
+const BITGESELL_NODE_RPC = 'rpc.bglwallet.io';
+const BITGESELL_EXPLORER_API = 'https://api.bitaps.com/bgl/v1/blockchain'
 
 /**
  * Querying address by making api calls to the the Bitgesell API
@@ -11,12 +13,16 @@ const BITGESELL_EXPLOER_API = 'https://api.bitaps.com/bgl/v1/blockchain'
 async function getAddressBalance(address) {
     try {
         const headers = {}
-        const res = await axios.ge()
+        const res = await axios.get(`${BITGESELL_EXPLORER_API}/address/transactions/${address}`)
+        const { data } = res.data
+        return data
     } catch (error) {
-
+        console.log(error)
     }
 }
 
-(() => {
-
+(async () => {
+    const address = 'bgl1qlmzckh904vze03n0lwzptt5dkmvf2vj3ev4qm9'
+    const address_balance = await getAddressBalance(address)
+    console.log(address_balance)
 })()
